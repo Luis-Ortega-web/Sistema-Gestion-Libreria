aaf8a1a5383b40fe400de718484ec244586348fd
Anthony Medina <alcantarawilme2@gmail.com>
2026-03-01 19:31:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.data.database_mgr import create_tables
+from src.presentation.login_ui import login
 
 if __name__ == "__main__":
     create_tables()
-    print("Base de datos creada correctamente")
+
+    if login():
+        print("Bienvenido al sistema")
+    else:
+        print("Acceso denegado")
