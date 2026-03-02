0b14acdfcc8710927d6825fcf16bb1479392cb75
Anthony Medina <alcantarawilme2@gmail.com>
2026-03-01 20:19:41 -0400

    Implementación módulo login e inventario

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from src.data.database_mgr import create_tables
+from src.data.database_mgr import create_tables, get_connection
 from src.presentation.login_ui import login
+from src.presentation.inventory_ui import menu_inventario
+
+def crear_usuario_prueba():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    
+    cursor.execute("SELECT * FROM usuario WHERE username = ?", ("admin",))
+    usuario = cursor.fetchone()
+
+    if not usuario:
+        cursor.execute("""
+            INSERT INTO usuario (username, password, nombre, rol)
+            VALUES (?, ?, ?, ?)
+        """, ("admin", "1234", "Administrador", "admin"))
+        conn.commit()
+
+    conn.close()
 
 if __name__ == "__main__":
     create_tables()
+    crear_usuario_prueba()
 
     if login():
         print("Bienvenido al sistema")
+        menu_inventario()
     else:
         print("Acceso denegado")
